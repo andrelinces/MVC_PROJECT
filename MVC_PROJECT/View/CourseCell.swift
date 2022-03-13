d3015943c124ff8d5cde4d087096425d285abb04
Andre Linces <andrelinces@yahoo.com.br>
2022-03-13 10:43:25 -0300

    Auto Layout, customization cell.

--- MVC_PROJECT/View/CourseCell.swift
+++ MVC_PROJECT/View/CourseCell.swift
@@ -26,5 +26,51 @@ class CourseCell: UITableViewCell {
             }
         }
     }
+    //MARK: - CONFIGURATION OF AUTO LAYOUT
+    
+    //Name title customization
+    override func setHighlighted(_ highlighted: Bool, animated: Bool) {
+        super.setHighlighted(highlighted, animated: animated)
+        contentView.backgroundColor = isHighlighted ? .lightText : .white
+        textLabel?.textColor = isHighlighted ? UIColor.white : .blue
+        detailTextLabel?.textColor = isHighlighted ? .white : .black
+    }
+
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+
+        //cell customization
+        textLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+        textLabel?.numberOfLines = 0
+        detailTextLabel?.textColor = .black
+        detailTextLabel?.font = UIFont.systemFont(ofSize: 20, weight: .light)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    
+//    override func setHighlighted(_ highlighted: Bool, animated: Bool) {
+//        super.setHighlighted(highlighted, animated: animated)
+//        contentView.backgroundColor = isHighlighted ? .highlightColor : .white
+//        textLabel?.textColor = isHighlighted ? UIColor.white : .mainTextBlue
+//        detailTextLabel?.textColor = isHighlighted ? .white : .black
+//    }
+//
+//    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+//        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+//
+//        // cell customization
+//        textLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+//        textLabel?.numberOfLines = 0
+//        detailTextLabel?.textColor = .black
+//        detailTextLabel?.font = UIFont.systemFont(ofSize: 20, weight: .light)
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError()
+//    }
       
 }
