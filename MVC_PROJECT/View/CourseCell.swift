27c05773b83fb9dabe942642134ce34180e6fc9e
Andre Linces <andrelinces@yahoo.com.br>
2022-03-13 09:56:57 -0300

    Logic to receive JSON data and display in cells. New file Course.

--- MVC_PROJECT/View/CourseCell.swift
+++ MVC_PROJECT/View/CourseCell.swift
@@ -8,8 +8,23 @@
 import UIKit
 
 class CourseCell: UITableViewCell {
-    
-    
-    
-    
+    //Logic to receive JSON data and display in cells.
+    var course: Course! {
+        
+        didSet {
+            
+            textLabel?.text = course.name
+            
+            if course.numberOfLessons > 35 {
+                
+                accessoryType = .detailDisclosureButton
+                detailTextLabel?.text = "Lessons 30+ Check it Out!"
+            }else{
+                
+                detailTextLabel?.text = "Lessons: \(course.numberOfLessons)"
+                accessoryType = .none
+            }
+        }
+    }
+      
 }
